3f504e3f0aac88aa86f11da8f051af81d37311bf
kayatsa <kayatsa8@gmail.com>
2025-05-08 19:27:00 +0300

    Dweller: isMarried() + marry()

--- Server/fallout/src/main/java/com/fallout/fallout/model/Dweller.java
+++ Server/fallout/src/main/java/com/fallout/fallout/model/Dweller.java
@@ -1,7 +1,11 @@
 package com.fallout.fallout.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.fallout.fallout.exception.AlreadyMarriedException;
+
+import jakarta.annotation.Nullable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -39,19 +43,38 @@ public class Dweller {
 
     // family
     @Min(value = -1, message = "the father's id is invalid")
+    @Nullable
     private Integer fatherId;
 
     @Min(value = -1, message = "the mother's id is invalid")
+    @Nullable
     private Integer motherId;
 
     @Min(value = 0, message = "the spouse's id is invalid")
+    @Nullable
     private Integer spouseId;
 
     // will be created by the system
     private List<Integer> childrenIds;
 
     // work
-    @NotNull(message = "the room of the dweller cannot be null")
     private Room room;
     
+
+    public boolean isMarried(){
+        return spouseId != null;
+    }
+
+    public void marry(Dweller spouse) throws AlreadyMarriedException{
+        if(!isMarried()){
+            spouseId = spouse.id;
+            childrenIds = new ArrayList<>(1);
+
+            if(gender == Gender.FEMALE){
+                lastName = spouse.lastName;
+            }
+        }
+
+        throw new AlreadyMarriedException(this);
+    }
 }
