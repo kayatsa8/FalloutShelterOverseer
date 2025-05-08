7604340fa25480b2a8c8e8dd8596eda65f81d4b2
kayatsa <kayatsa8@gmail.com>
2025-05-08 18:16:20 +0300

    Server/fallout/../model/Dweller: fields

--- Server/fallout/src/main/java/com/fallout/fallout/model/Dweller.java
+++ Server/fallout/src/main/java/com/fallout/fallout/model/Dweller.java
@@ -1,5 +1,22 @@
 package com.fallout.fallout.model;
 
+import java.util.List;
+
 public class Dweller {
+    // private
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private Integer generation;
+
+    // family
+    private Integer fatherId;
+    private Integer motherId;
+    private Integer spouseId;
+    private List<Integer> childrenIds;
+
+    // work
+    private Room room;
     
 }
