80518506c01eb49ba4eee5782ecc8ff0713df145
kayatsa <kayatsa8@gmail.com>
2025-05-08 17:43:18 +0300

    init server

---
+++ Server/fallout/src/main/java/com/fallout/fallout/FalloutApplication.java
@@ -0,0 +1,13 @@
+package com.fallout.fallout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalloutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalloutApplication.class, args);
+	}
+
+}
---
+++ Server/fallout/src/test/java/com/fallout/fallout/FalloutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fallout.fallout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalloutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
